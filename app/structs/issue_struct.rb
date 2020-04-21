b3628138797fa4d7c5b2fde70f6ff4d51445d5fb
Liubomyr <lyubomur.user.mv@gmail.com>
2020-04-21 17:15:30 +0300

    Fix offense

--- app/structs/issue_struct.rb
+++ app/structs/issue_struct.rb
@@ -1,22 +1,15 @@
 IssueStruct = Struct.new(
-  :github_id,
-  :title,
-  :state,
-  :locked,
-  :number,
-  :html_url,
-  :body,
-  :updated_on_github_at,
-  :user_json,
-  :assignee_json
+  :github_id, :title,
+  :state, :locked,
+  :number, :html_url,
+  :body, :updated_on_github_at,
+  :user_json, :assignee_json
 ) do
   class << self
     def from_json(json)
-      IssueStruct.new(
-        json[:id], json[:title], json[:state], json[:locked],
-        json[:number], json[:html_url], json[:body], json[:updated_at],\
-        json[:user], json[:assignee]
-      )
+      IssueStruct.new(json[:id], json[:title], json[:state], json[:locked],
+                      json[:number], json[:html_url], json[:body], json[:updated_at],
+                      json[:user], json[:assignee])
     end
 
     def from_url(url)
@@ -36,10 +29,8 @@ IssueStruct = Struct.new(
   end
 
   def to_hash
-    {
-      github_id: github_id, title: title, state: state,
+    { github_id: github_id, title: title, state: state,
       locked: locked, number: number, html_url: html_url,
-      updated_on_github_at: updated_on_github_at
-    }
+      updated_on_github_at: updated_on_github_at }
   end
 end
