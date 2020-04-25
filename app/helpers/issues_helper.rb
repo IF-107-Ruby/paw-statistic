199f662e0beb14ebdf55cfe8bb90a731df138330
Liubomyr <lyubomur.user.mv@gmail.com>
2020-04-26 00:10:25 +0300

    Move issues decorated_state method from decorator to helper

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,11 +1,9 @@
-class IssueDecorator < Draper::Decorator
-  delegate_all
-
-  def decorated_state
+module IssuesHelper
+  def decorated_state(issue)
     state_style = case issue.state
                   when 'open' then 'text-success'
                   when 'closed' then 'text-danger'
                   else ''; end
-    h.tag.p(issue.state, class: 'm-0 ' + state_style)
+    tag.p(issue.state, class: 'm-0 ' + state_style)
   end
 end
